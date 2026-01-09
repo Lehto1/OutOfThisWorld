d87708b02cc9f4e95b2c8e637286481314360bf1
Elliot-JW-Jordan <elliot.jordan@elev.ga.lbs.se>
2026-01-09 14:48:54 +0100

    Fixx

--- Assets/Script/VirusStage/VirusStage3.cs
+++ Assets/Script/VirusStage/VirusStage3.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class VirusStage1 : MonoBehaviour
+public class VirusStage3 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
