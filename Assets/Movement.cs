0f359df358412820578c0f722ed4fac6334c4088
Lehto1 <harry.lehto13@gmail.com>
2026-01-12 13:18:48 +0100

    MovementCode

---
+++ Assets/Movement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class Movement : MonoBehaviour
+{
+    int Running = 1; //Variabel som ändrar på spelarens hastighet
+    
+
+    Rigidbody RB;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RunningOrNot();
+        MovementStuff();
+    }
+
+    void MovementStuff() //Includes keybinds and code for moving the player
+    {
+        RB.angularVelocity = new Vector3(0, 0, 0);
+        RB.linearVelocity = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) // If sats for att go bakåt och framåt
+        {
+            RB.linearVelocity = Running * transform.forward;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            RB.linearVelocity = Running * -transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.A)) //If sats for rotation
+        {
+            RB.angularVelocity = new Vector3(0, -10, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RB.angularVelocity = new Vector3(0, 10, 0);
+        }
+    }
+
+    void RunningOrNot()
+    {
+        if (Input.GetKey(KeyCode.LeftShift)) //Används för att ändra spelarens hastighet när de springer
+        {
+            Running = 4;
+        }
+        else
+        {
+            Running = 2;
+        }
+    }
+}
