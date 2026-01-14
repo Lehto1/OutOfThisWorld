5a424d3f7f4f3c4b78781b81bf1ba12c473c5306
Elliot-JW-Jordan <elliot.jordan@elev.ga.lbs.se>
2026-01-14 15:35:58 +0100

    [Inte Testad] Grundlig insekt. Kan Patrullera och jaga? I

--- Assets/Script/AIPathfinding.cs
+++ Assets/Script/AIPathfinding.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.AI;
@@ -107,7 +108,7 @@ public abstract class AIPathfinding : MonoBehaviour
         NavMeshInit();
 
         //Skriver till konsollen
-        Debug.Log("lll");
+      //  Debug.Log("lll");
         //Eventuell animation
         //Eventuellt ljud
     }
@@ -126,7 +127,7 @@ public abstract class AIPathfinding : MonoBehaviour
         //Initialiserar spelarens start tillstånd
         AIstateInit();
 
-        Debug.Log($"AI Init, State : {currentAIState} Speed : {aiMovementSPeed} ");
+        UnityEngine.Debug.Log($"AI Init, State : {currentAIState} Speed : {aiMovementSPeed} ");
 
 
     }
@@ -139,12 +140,12 @@ public abstract class AIPathfinding : MonoBehaviour
         // Ifall navAgenten finns kommer koden nedan inte att köras
         if (navAgent == null)
         {
-            Debug.LogError("The game does not have a NavMesh co");
+            UnityEngine.Debug.LogError("The game does not have a NavMesh co");
             enabled = false;
             return;
 
         }
-        Debug.Log("AIpathf required Navmesh ");
+        UnityEngine.Debug.Log("AIpathf required Navmesh ");
     }
 
   //Konfigurerar NavMesh inställningar, sätter dessa till värdena utav klasses egna variabler nedan
@@ -165,7 +166,7 @@ public abstract class AIPathfinding : MonoBehaviour
         //sätt på agenten
         navAgent.enabled = true;
 
-        Debug.Log($"navAget has been Configurated {gameObject.name} sPEED {navAgent.speed} TURNSPEED {navAgent.angularSpeed}");
+        UnityEngine.Debug.Log($"navAget has been Configurated {gameObject.name} sPEED {navAgent.speed} TURNSPEED {navAgent.angularSpeed}");
 
 
     }
@@ -176,7 +177,7 @@ public abstract class AIPathfinding : MonoBehaviour
         //ifall den är det, Returernar koden
         if(playerTransformTarget != null)
         {
-            Debug.Log("Player already found, returning....");
+            UnityEngine.Debug.Log("Player already found, returning....");
             return;
         }
 
@@ -187,12 +188,12 @@ public abstract class AIPathfinding : MonoBehaviour
             //sätter ai:n target position till spelar objektets transform
             playerTransformTarget = playerOBJ.transform;
 
-            Debug.Log($"Player was found by tag{playerOBJ.name}");
+            UnityEngine.Debug.Log($"Player was found by tag{playerOBJ.name}");
         }
         else
         {
             //Debuggr
-            Debug.LogWarning($"Player could no be found");
+            UnityEngine.Debug.LogWarning($"Player could no be found");
           
         }
 
@@ -207,7 +208,7 @@ public abstract class AIPathfinding : MonoBehaviour
             //Efter som koden har några waypoints utsatta, så kan Ai:n nu börja patrullera
             ChangeState(AiState.Patrol);  //byter AI tillstånd
             DecideNextWaypoint();
-            Debug.Log($"Ai kommer starta I sitt patrullerings tillstånd");
+            UnityEngine.Debug.Log($"Ai kommer starta I sitt patrullerings tillstånd");
 
         }
         else
@@ -215,7 +216,7 @@ public abstract class AIPathfinding : MonoBehaviour
             //AI:n kommer förbli Idle 
             //Eftersom det inte finns några punkter för AI:n att gå efter
             ChangeState(AiState.Idle);
-            Debug.Log("AI will start in IDLE");
+            UnityEngine.Debug.Log("AI will start in IDLE");
 
         }
 
@@ -258,7 +259,7 @@ public abstract class AIPathfinding : MonoBehaviour
     //TImern öker så länge ai:n beffineer sig i ett vist tillstånd.
     protected virtual void UpdateTimer()
     {
-      stateTimer  += Time.deltaTime;
+      stateTimer += Time.deltaTime;
 
     }
 
@@ -297,7 +298,7 @@ protected virtual void UpdatePointWaitTimer()
             detectedPlayer = true;
             mostRecentPlayerPOS = playerTransformTarget.position;
 
-            Debug.Log($"Player detected");
+            UnityEngine.Debug.Log($"Player detected");
 
             //byter Ai:s tillstpnd
             //bör nu börja jaga spelaren
@@ -309,7 +310,7 @@ protected virtual void UpdatePointWaitTimer()
             //Ai har inte lägre spelaren i sikte
             detectedPlayer = false;
 
-            Debug.Log($"AI {gameObject.name} lost sight of the player");
+            UnityEngine.Debug.Log($"AI {gameObject.name} lost sight of the player");
 
             //ifall Ai:n inte längre ser spelaren, ska AI: gå till spelarens senast kända 
             //position
@@ -459,8 +460,9 @@ protected virtual void UpdatePointWaitTimer()
         if (aiPatrolWaypoints == null || aiPatrolWaypoints.Length == 0)
         {
             return; // de finns inga
-            navAgent.SetDestination(aiPatrolWaypoints[aiPatrolWaypointsIndex].position); // Nav 
+           
         }
+        navAgent.SetDestination(aiPatrolWaypoints[aiPatrolWaypointsIndex].position); // Nav 
     }
 
     //navAgent börjar röra sig mor spelaren 
@@ -478,14 +480,78 @@ protected virtual void UpdatePointWaitTimer()
     //sTATECHANGER, //Byter Ai:s tillstånd 
     protected virtual void ChangeState(AiState newState)
     {
+        //gör ingeting om det nuvarnade tillstååndet är det samma som det nya
+        if(currentAIState == newState)
+        {
+            return;
+        }
+
+        //det gamla, förredetta tillståndet uppdateras
+        previousAIState = currentAIState;
+
+        //uppdatera  det nuvarande till det nya tillståndet
+        currentAIState = newState;
+
+        stateTimer = 0f; // eftersom koden har bytt 
+
+        //nollställer även waypoint timern
+        aiPointTImer = 0f;
+
+        UnityEngine.Debug.Log($"AI {gameObject.name} has changed state from {previousAIState} to {currentAIState} ");
 
     }
+
+    //huvud metod 
+    //Kör  logic baserat på tillstånd
+
+
     protected virtual void Execute()
     {
-        
+        //switchar baserat på nuvarande tillstånd
+        switch(currentAIState) 
+        {
+            case AiState.Idle: //Vid Idle
+                ExecuteIdle();
+                break;
+            case AiState.Patrol: //vid patrullering
+                ExecutePatrol();
+                break;
+            case AiState.Chase: // Vid jakt
+                ExecuteChase();
+                break;
+            case AiState.Attack: // Vid attack
+                ExecuteAttack();
+                break;
+            case AiState.Dead: // Vid död
+                ExecuteDeath();
+                break;
+
+            default: break; //inget
+        }
+    }
+    
+    //helper metoder som ska implementeras och fyllas utav barnklasserna
+  protected virtual void  ExecuteIdle()
+    {
+
+    }
+    protected virtual void ExecutePatrol()
+    {
+
+    }
+    protected virtual void ExecuteChase()
+    {
+
     }
 
+    protected virtual void ExecuteAttack()
+    {
 
+    }
+    protected virtual void ExecuteDeath()
+    {
+
+    }
     //Barnklassernas olika egenskaper
     protected abstract void UniqueBehavior();
  
--- Assets/Script/Insects.cs
+++ Assets/Script/Insects.cs
@@ -7,38 +7,71 @@ using UnityEngine;
 
 namespace Assets.Script
 {
-    internal class Insects : EnemyPathMovement
+    internal abstract class Insects : AIPathfinding
     {
-        [Header("Insect Settings")]
-        public float health = 50f;
+        [Header("InsectSettings")]
+        [SerializeField] private int insectDMG = 5; //dess skada
+        [SerializeField] private float insectAttackCoolDown = 1.5f; // colldown
+        private float insectAttackTimer = 0f; //tidtagning
 
-        void Start()
+        protected override void ExecuteIdle()
         {
-            speed = 4.5f; // Insekter är snabbare
+            //insekten står stilla, gör inget 
+            //väntar på händelser
+            navAgent.velocity = Vector3.zero;
+            
         }
-
-      //  protected override void OnPathComplete()
-       // {
-            // Insekter dör när de når slutet
-          //  currentWaypointIndex = 0;
-
-      //  }
-
-        public void TakeDamage(float damage)
+        protected override void ExecutePatrol()
         {
-            health -= damage;
-
-            if (health <= 0)
+            //Undersöker om insekten har nått "waypointen" 
+            if(navAgent.remainingDistance < aiPointTole)
             {
-                Die();
+                //insekten har nu kommit tillräkligt nära waypointen för att det 
+                //det ska räknas att AI; har varit där 
+                if(aiPointTImer >= aiTimeAtPoint)
+                {
+                    //Börjar färd mot nästa waypoint
+                    aiPatrolWaypointsIndex++;
+
+                    //Om Ai:n har passerat alla waypoints , ska det gå tilbaka till den första startpunkten
+                    if (aiPatrolWaypointsIndex >= aiPatrolWaypoints.Length)
+                    {
+                        aiPatrolWaypointsIndex = 0;
+
+                    }
+                    //ställer om timern
+                    aiPointTImer = 0f;
+
+                    //bestäm nästa waypoint
+                    DecideNextWaypoint();
+
+                    Debug.Log($"THe insect {gameObject.name} moving to its next waypoint : {aiPatrolWaypointsIndex}");
+
+
             }
-        }
-
-        void Die()
-        {
-            currentWaypointIndex = 0;
-
         }
     }
 
-}
+        protected override void ExecuteChase()
+        {
+            // börjar röra sig mot spelarens senas kända pos
+            navAgent.SetDestination(mostRecentPlayerPOS);
+
+            //ifall insekten får syn på spelaren så..
+            if (detectedPlayer && playerTransformTarget != null)
+            {
+                //uppdateras destinationen till den aktuella spelarpositionen
+                navAgent.SetDestination(playerTransformTarget.position);
+            }
+            
+
+            //kollar om insekten äär nära nog spelaren för att mangla 
+            if(distanceToTarget <= aiAttackRadius)
+            {
+                ChangeState(AiState.Attack);
+            }
+
+            }
+        }
+
+    }
