99eb764b65ff4a847c91083ea2c2f0bd0f2f585e
Lehto1 <harry.lehto13@gmail.com>
2026-01-14 13:11:10 +0100

    Models

--- Assets/Camera follow.cs
+++ Assets/Camera follow.cs
@@ -1,26 +1,29 @@
+using TMPro;
 using UnityEngine;
 
 public class Camerafollow : MonoBehaviour
 {
-    public Transform player;      // Spelaren som kameran följer
-    public Vector3 offset;        // Avstånd bakom/ovanför spelaren
-    public float smoothSpeed = 5f;
+    [SerializeField] Transform player;      // Spelaren som kameran följer
+    [SerializeField] Vector3 offset;        // Avstånd bakom/ovanför spelaren
+    [SerializeField] float smoothSpeed = 5f;
+    Vector3 Velocity;
 
     void Update()
     {
-        // Önskad position bakom spelaren
-        Vector3 targetPosition = player.position + 5 * -transform.forward;
 
         // Kameran har samma rotation som spelaren
         transform.rotation = player.rotation;
 
         // Mjuk följning
         // Mjuk följning
-        transform.position = Vector3.Lerp(
-            transform.position,
-            targetPosition,
-            smoothSpeed * Time.deltaTime
-        );
+        // transform.position = Vector3.Lerp(
+        // transform.position,
+        // player.position - offset,
+        // smoothSpeed * Time.deltaTime
+        // );
+
+        transform.position = player.position + 5 * -transform.forward;
+
 
     }
 }
