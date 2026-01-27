2c6fd3ab81969b4741a555638613fd7a22179a43
Elliot-JW-Jordan <elliot.jordan@elev.ga.lbs.se>
2026-01-27 20:27:30 +0100

    Förbättrade kamerakoden

--- Assets/Script/Camera follow.cs
+++ Assets/Script/Camera follow.cs
@@ -4,26 +4,55 @@ using UnityEngine;
 public class Camerafollow : MonoBehaviour
 {
     [SerializeField] Transform player;      // Spelaren som kameran följer
-    [SerializeField] Vector3 offset;        // Avstånd bakom/ovanför spelaren
-    [SerializeField] float smoothSpeed = 5f;
-    Vector3 Velocity;
+    [SerializeField] Vector3 offset = new Vector3(0, 1.5f, -4f);        // Avstånd bakom/ovanför spelaren
+    [SerializeField] float positionSmoothSpeed = 5f;
+    [SerializeField] float rotationSmoothSpeed = 10f;
+    [SerializeField] LayerMask collisionMask;
+    [SerializeField] float minumumCollisionDistance = 0.8f;
+
+    private Vector3 Velocity;
+    private float currentDistance; // Nuvariga avstånd
 
     void Update()
     {
+        //Kontrollerar så att spelaren finns
+        if (player == null)
+        {
+            return;
+        }
+
+        //Beräknar målpositionen
+        Vector3 targPos = player.position + player.TransformDirection(offset);
+
+        //kontrollera kollisionen
+        //flyttar närmare om något träffas
+        Vector3 directionToCamera = (targPos - player.position).normalized;
+
+        float wantedDistance = offset.magnitude;
+        currentDistance = wantedDistance;
+
+        //Raycast
+        if (Physics.Raycast(player.position, directionToCamera, out RaycastHit hit, wantedDistance, collisionMask))
+        {
+            currentDistance = Mathf.Max(hit.distance - minumumCollisionDistance, 0.3f);
+        }
+
+        //uppdatera targ pos baserat på distansen
+        targPos = player.position + directionToCamera * currentDistance;
+
+        //mjuk
+        transform.position = Vector3.SmoothDamp(transform.position, targPos, ref Velocity, 1f / positionSmoothSpeed);
+
+        //Kammeran tittar på spelaren
+        Vector3 lookatPlayer = player.position + Vector3.up * 0.6f;
+        //över huvudet 
+        Vector3 dirrectionToLook = (lookatPlayer - transform.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(dirrectionToLook);
 
         // Kameran har samma rotation som spelaren
-        transform.rotation = player.rotation;
-
-        // Mjuk följning
-        // Mjuk följning
-        // transform.position = Vector3.Lerp(
-        // transform.position,
-        // player.position - offset,
-        // smoothSpeed * Time.deltaTime
-        // );
-
-        transform.position = player.position + 5 * -transform.forward;
-
-
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSmoothSpeed * Time.deltaTime);
     }
 }
+     
+
+ 
